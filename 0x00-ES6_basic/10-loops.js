b5afca8c0fc2a9f99cc3613bd8f68605911c82a4
josiah-praise <praisechinonso21@gmail.com>
2024-07-23 09:29:05 +0100

    10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 /* eslint no-param-reassign: "error" */
+/* eslint-disable */
 
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [index, element] of array.entries()) {
+  let index = 0;
+
+  for (const element of array) {
     array[index] = appendString + element;
+    index += 1;
   }
 
   return array;
