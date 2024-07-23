dcf0f4b7ea40719946dc265eb51de8a8501d23f6
josiah-praise <praisechinonso21@gmail.com>
2024-07-23 01:55:35 +0100

    7-getBudgetObject.js

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+/* eslint-disable */
+
+
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
