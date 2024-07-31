48f357d1a5e3a0acf92482349b060770134cef4b
josiah-praise <praisechinonso21@gmail.com>
2024-07-31 14:35:44 +0100

    3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,5 +2,5 @@
 
 export default function getStudentIdsSum(arr) {
   if (!Array.isArray(arr)) return [];
-  return arr.reduce((ele, acc) => acc + ele.id, 0);
+  return arr.reduce((acc, obj) => acc + obj.id, 0);
 }
