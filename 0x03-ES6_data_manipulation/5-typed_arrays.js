9642f28384216b5da46e3146526294c7c123d580
josiah-praise <praisechinonso21@gmail.com>
2024-08-01 10:20:07 +0100

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,7 +5,7 @@ export default function createInt8TypedArray(length, position, value) {
     return new DataView(new ArrayBuffer());
   }
 
-  if (position <= 0 || position >= length) throw new Error('Position outside range');
+  if (position < 0 || position >= length) throw new Error('Position outside range');
 
   const arrayBuffer = new ArrayBuffer(length);
   const dataView = new DatatView(arrayBuffer);
