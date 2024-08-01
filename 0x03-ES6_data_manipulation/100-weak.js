c389557b4a515058722219b4524f53b9bf0b98ce
josiah-praise <praisechinonso21@gmail.com>
2024-08-01 13:00:29 +0100

    100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,7 +4,7 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
-    if (weakMap.get(endpoint) < 5) {
+    if (weakMap.get(endpoint) + 1 < 5) {
       weakMap.set(endpoint, weakMap.get(endpoint) + 1);
     } else {
       throw new Error('Endpoint load is high');
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 export default function cleanSet(set, startString) {
-  if (!(set instanceof Set) || typeof startString !== 'string') return '';
+  if (!(set instanceof Set) || typeof startString !== 'string' || startString.length === 0) return '';
 
   const remainder = [];
 
