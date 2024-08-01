c11186f7905a4a780287bcbc9207e5ee295ef5ac
josiah-praise <praisechinonso21@gmail.com>
2024-08-01 11:10:51 +0100

    10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
 export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) return new Map();
-
-  if (map.size === 0) throw new Error('Cannot process');
+  if (!(map instanceof Map)) throw new Error('Cannot process');
 
   for (const [key, value] of map) {
     if (value === 1) map.set(key, 100);
