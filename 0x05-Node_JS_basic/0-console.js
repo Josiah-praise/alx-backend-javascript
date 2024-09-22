24d2c4f7f76fb0b64506289dad9193838b34785b
josiah-praise <praisechinonso21@gmail.com>
2024-09-22 06:09:49 +0100

    node basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Josiah Praise <https://github.com/josiah-praise/>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
